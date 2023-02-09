7ff87ddbb5eb13486e534407c7a2275b679e4641
Alain Forton <495365+alainforton@users.noreply.github.com>
2023-02-09 17:27:11 -0600

    test 01 bit_core

--- general/init.js
+++ general/init.js
@@ -1,12 +1,21 @@
 
 
 // tres test 3T
+let estatus = ['testing','TODO','done','urgente'];
 
-let t = 'test';
+let tiempo = new Date();
+tiempo.date = tiempo.toLocaleDateString();
+tiempo.time = tiempo.toLocaleTimeString();
 
-document.title = t;
-console.log(t);
+let t = 'mejorar testing demomento solo para index y github';
+
+document.title = estatus[0];
+console.log(t, tiempo.time, tiempo.date);
 //prompt(t);
+
+// hacer bit_core a la par. enlazar localStorage para crear archivos de texto, descargarlos (generar archivo);
+// buscar limite de memoria, demomento limite = qr
+
 
 // mejorar testing demomento solo para index y github
 
